d1734da12dc88f9d9dcb1471fa549736db7239f2
zhenglz <astrozheng@gmail.com>
2018-10-08 00:27:47 +0800

    update readme and setup.py

---
+++ README.md
@@ -0,0 +1,35 @@
+# SHA stock market analysis tools and tutorials
+<br></br>
+## Functions (TODO):
+#### 1. Collect stock market data
+#### 2. Correlations between different stocks
+#### 3. Prediction models
+#### 4. Trading strategy
+
+## Installation
+#### 1. using pip
+<p>
+$ git clone https://github.com/zhenglz/stockMarket.git
+<p>
+$ cd stockMarket
+<p>
+$ pip install .
+
+#### 2. using conda
+<p> To be completed
+
+## Dependencies
+<p> numpy
+<p> pandas
+<p> sklearn
+<p> matplotlib
+<p> tushare </p>
+
+#### How to install tushare
+To be completed
+
+## Examples
+To be completed
+
+## Tutorials (ipython notebook)
+To be completed
---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+"""
+Description of how to make a python package
+
+https://python-packaging.readthedocs.io/en/latest/command-line-scripts.html
+
+"""
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(name='stockMarket',
+      version='1.0',
+      long_description=readme(),
+      description='SHA stock market analysis tools and tutorials',
+      url='https://github.com/zhenglz/stockMarket',
+      author='zhenglz',
+      author_email='zhenglz@outlook.com',
+      license='GPL-3.0',
+      packages=['datatools', ],
+      install_requires=[
+          'numpy',
+          'pandas',
+          'sklearn',
+          'matplotlib',
+          'tushare',
+      ],
+      include_package_data=True,
+      zip_safe=False,
+      )
